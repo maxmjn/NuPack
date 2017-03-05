5fc7a47b37904377c9b145e8c1f92056b474dd68
maxmjn20 <murali.janardhanan@teamaol.com>
2017-03-05 14:28:07 -0500

    Updated README Adding lookup table for various markup percent

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # NuPack
+- Final cost calculator library applies markup percent on given base price to output final cost
+- Expected input are BasePrice, Number of Persons, Category
+- Category can be Food,
+
+#### Support
+- dev@nupack.com
+
+#### Setup
+
+ - Java 1.8
+ - Maven 3.3.x
+ - git clone https://github.com/maxmjn/NuPack.git
+
+#### Build JAR
+ - from terminal mvn clean install
+ - JAR file created
+ 
+#### Usage
+ ```Java
+import com.nupack.MarkUpCalculator;
+public class App{
+    public static void main(String[] args){
+        Double finalCost = MarkUpCalculator.calculateFinalCost(basePrice, numPersons, category);
+    }
+}
+```
--- src/main/java/com/nupack/MarkUpCalculator.java
+++ src/main/java/com/nupack/MarkUpCalculator.java
@@ -1,5 +1,8 @@
 package com.nupack;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Calculate final cost by adding appropriate markup prices to provided based price
  *
@@ -7,8 +10,24 @@ package com.nupack;
 public class MarkUpCalculator
 {
 
+    private static Map<String, Double> categoryMarkUpPercent = new HashMap<String, Double>();
+
+    /**
+     * Initialize Map with Category pricing logic
+     */
+    static {
+        categoryMarkUpPercent.put("flat", 0.05);
+        categoryMarkUpPercent.put("persons", 0.012);
+        categoryMarkUpPercent.put("pharmaceuticals", 0.075);
+        categoryMarkUpPercent.put("food", 0.13);
+        categoryMarkUpPercent.put("electronics", 0.02);
+    }
+
     public static final Double calculateFinalCost(Double basePrice, int numPersons, String category){
 
+        /**
+         * Input validation
+         */
         if(basePrice == null || basePrice <= 0){
            throw new RuntimeException("BasePrice is Invalid");
         }
@@ -18,6 +37,10 @@ public class MarkUpCalculator
         if(category == null || category.trim().length() == 0){
             throw new RuntimeException("Category is Invalid");
         }
+
+        /**
+         * Get pricing for category
+         */
         return 0.0;
     }
 }
